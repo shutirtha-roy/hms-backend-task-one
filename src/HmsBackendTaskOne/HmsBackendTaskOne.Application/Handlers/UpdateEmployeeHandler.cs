f1908cdb6cc12eb632956076ca2f3c802da76d5f
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-15 04:28:16 +0600

    Automapper added(code refactored)

--- src/HmsBackendTaskOne/HmsBackendTaskOne.Application/Handlers/UpdateEmployeeHandler.cs
+++ src/HmsBackendTaskOne/HmsBackendTaskOne.Application/Handlers/UpdateEmployeeHandler.cs
@@ -1,4 +1,5 @@
-﻿using HmsBackendTaskOne.Application.Commands;
+﻿using AutoMapper;
+using HmsBackendTaskOne.Application.Commands;
 using HmsBackendTaskOne.Application.Queries;
 using HmsBackendTaskOne.Domain.Entities;
 using HmsBackendTaskOne.Domain.IUnitOfWorks;
@@ -9,21 +10,18 @@ namespace HmsBackendTaskOne.Application.Handlers
     public class UpdateEmployeeHandler : IRequestHandler<UpdateEmployeeCommand, Employee>
     {
         private readonly IApplicationUnitOfWork _applicationUnitOfWork;
+        private readonly IMapper _mapper;
 
-        public UpdateEmployeeHandler(IApplicationUnitOfWork applicationUnitOfWork)
+        public UpdateEmployeeHandler(IApplicationUnitOfWork applicationUnitOfWork, IMapper mapper)
         {
             _applicationUnitOfWork = applicationUnitOfWork;
+            _mapper = mapper;
         }
 
         public async Task<Employee> Handle(UpdateEmployeeCommand request, CancellationToken cancellationToken)
         {
             var employee = _applicationUnitOfWork.Employees.GetById(request.Employee.Id);
-            employee.Name = request.Employee.Name;
-            employee.Email = request.Employee.Email;
-            employee.Designation = request.Employee.Designation;
-            employee.Phone = request.Employee.Phone;
-            employee.Salary = request.Employee.Salary;
-            employee.Department = request.Employee.Department;
+            employee = _mapper.Map(request.Employee, employee);
 
             _applicationUnitOfWork.Save();
 
---
+++ src/HmsBackendTaskOne/HmsBackendTaskOne.Application/Profiles/ApplicationProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HmsBackendTaskOne.Domain.Entities;
+
+namespace HmsBackendTaskOne.Application.Profiles
+{
+    public class ApplicationProfile : Profile
+    {
+        public ApplicationProfile()
+        {
+            CreateMap<Employee, Employee>();
+        }
+    }
+}
