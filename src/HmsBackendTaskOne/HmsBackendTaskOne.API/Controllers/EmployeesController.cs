c9bf1ac0dcdd0169c5bc1bcc8e18c3f6ba2c0f17
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-15 00:22:18 +0600

    EmployeesController Initialized with mediator

---
+++ src/HmsBackendTaskOne/HmsBackendTaskOne.API/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HmsBackendTaskOne.API.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public EmployeesController(IMediator mediator) => _mediator = mediator;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
